d3be6a2e8d31e5f9dc7c5ba70d16be403bd4046e
salararain <muhamadsalararain@gmail.com>
2026-04-12 20:17:30 +0500

    add form for new students and make it responsive

--- src/pages/AddNewStudent.jsx
+++ src/pages/AddNewStudent.jsx
@@ -1,9 +1,56 @@
 
 
 
-  const AddNewStudent = () =>{
-    return(
-        <h1>hello</h1>
-    )
+const AddNewStudent = () => {
+
+  return (
+    <div className="w-full h-[100vh] bg-white flex justify-center items-start">
+      <div className="w-[90%] h-[85vh] overflow-y-auto bg-white p-1 shadow-2xl m-2 rounded ">
+        <h1 className="text-center text-4xl text-left">Addmission From</h1>
+        <fieldset className="fieldset flex flex-row feildset-vertical">
+          <legend className="fieldset-legend text-2xl text-left  label">Name of Student's:</legend>
+          <input type="text" className="input  " placeholder="FIRST" />
+          <input type="text" className="input  " placeholder="LAST" />
+        </fieldset>
+        <fieldset className="fieldset flex flex-row">
+          <legend className="fieldset-legend text-2xl text-left  label">Class you want to apply for:</legend>
+          <input type="text" className="input w-full" />
+        </fieldset>
+        <fieldset className="fieldset flex flex-row">
+          <legend className="fieldset-legend text-2xl text-left  label">Student's DOB:</legend>
+          <input type="date" className="input w-full" />
+        </fieldset>
+        <fieldset className="fieldset flex flex-row feildset-vertical">
+          <legend className="fieldset-legend text-2xl text-left  label">Parent / Guardian Name :</legend>
+          <input type="text" className="input  " placeholder="FIRST" />
+          <input type="text" className="input  " placeholder="LAST" />
+        </fieldset>
+        <fieldset className="fieldset flex flex-row">
+          <legend className="fieldset-legend text-2xl text-left  label">Current Address:</legend>
+          <input type="text" className="input w-full" />
+        </fieldset>
+        <fieldset className="fieldset flex flex-row feildset-vertical">
+          <input type="text" className="input  " placeholder="City" />
+          <input type="text" className="input  " placeholder="Region" />
+        </fieldset>
+        <fieldset className="fieldset flex flex-row feildset-vertical">
+          <input type="text" className="input  " placeholder="Postal Code" />
+          <input type="text" className="input  " placeholder="Countery" />
+        </fieldset>
+        <fieldset className="fieldset flex flex-row">
+          <legend className="fieldset-legend text-2xl text-left  label">Phone Number:</legend>
+          <input type="number" className="input w-full" />
+        </fieldset>
+         <fieldset className="fieldset flex flex-row">
+          <legend className="fieldset-legend text-2xl text-left  label">Email:</legend>
+          <input type="email" className="input w-full" />
+        </fieldset>
+        <div className="w-full  h-auto flex justify-center items-center">   
+                <button className="btn btn-primary  label ">Submit</button>
+        </div>  
+
+      </div>
+    </div>
+  )
 }
- export default AddNewStudent
+export default AddNewStudent
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,7 @@
 const Login = () => {
     return (
         <div className="w-full h-[100vh] bg-white flex justify-center items-center">
-            <div className="w-96 h-80 bg-white rounded text-center p-1 shadow-xl">
+            <div className="w-96 h-80 bg-white rounded text-center p-1 m-2 shadow-xl">
                 <h1 className="text-2xl">Sign In</h1>
                 <fieldset className="fieldset flex justify-center items-center ">
                     <legend className="fieldset-legend text-left text-xl ">Email</legend>
@@ -15,9 +15,9 @@ const Login = () => {
                     <input  type="Password" className="input w-full" placeholder="Type here" />
                     {/* <p className="label">Optional</p> */}
                 </fieldset>
-                <button className="btn btn-primary w-full">Primary</button>
+                <button className="btn btn-primary w-full">Sign in</button>
                 <p className="text-sm text-gray-500 mt-2">
-                    Don't have an account? <a href="#" className="text-blue-500 hover:underline">Sign up</a>
+                    Don't have an account? <a href="./components/layout.jsx" className="text-blue-500 hover:underline">Sign up</a>
                 </p>
             </div>
         </div>
