80600780f614574e844d7caded58dbf31a3e0cd4
salararain <muhamadsalararain@gmail.com>
2026-04-13 15:23:32 +0500

    making responsive

--- src/pages/AddNewStudent.jsx
+++ src/pages/AddNewStudent.jsx
@@ -4,50 +4,31 @@
 const AddNewStudent = () => {
 
   return (
-    <div className="w-full h-[100vh] bg-white flex justify-center items-start">
-      <div className="w-[90%] h-[85vh] overflow-y-auto bg-white p-1 shadow-2xl m-2 rounded ">
-        <h1 className="text-center text-4xl text-left">Addmission From</h1>
-        <fieldset className="fieldset flex flex-row feildset-vertical">
-          <legend className="fieldset-legend text-2xl text-left  label">Name of Student's:</legend>
-          <input type="text" className="input  " placeholder="FIRST" />
-          <input type="text" className="input  " placeholder="LAST" />
-        </fieldset>
-        <fieldset className="fieldset flex flex-row">
-          <legend className="fieldset-legend text-2xl text-left  label">Class you want to apply for:</legend>
-          <input type="text" className="input w-full" />
-        </fieldset>
-        <fieldset className="fieldset flex flex-row">
-          <legend className="fieldset-legend text-2xl text-left  label">Student's DOB:</legend>
-          <input type="date" className="input w-full" />
-        </fieldset>
-        <fieldset className="fieldset flex flex-row feildset-vertical">
-          <legend className="fieldset-legend text-2xl text-left  label">Parent / Guardian Name :</legend>
-          <input type="text" className="input  " placeholder="FIRST" />
-          <input type="text" className="input  " placeholder="LAST" />
-        </fieldset>
-        <fieldset className="fieldset flex flex-row">
-          <legend className="fieldset-legend text-2xl text-left  label">Current Address:</legend>
-          <input type="text" className="input w-full" />
-        </fieldset>
-        <fieldset className="fieldset flex flex-row feildset-vertical">
-          <input type="text" className="input  " placeholder="City" />
-          <input type="text" className="input  " placeholder="Region" />
-        </fieldset>
-        <fieldset className="fieldset flex flex-row feildset-vertical">
-          <input type="text" className="input  " placeholder="Postal Code" />
-          <input type="text" className="input  " placeholder="Countery" />
-        </fieldset>
-        <fieldset className="fieldset flex flex-row">
-          <legend className="fieldset-legend text-2xl text-left  label">Phone Number:</legend>
-          <input type="number" className="input w-full" />
-        </fieldset>
-         <fieldset className="fieldset flex flex-row">
-          <legend className="fieldset-legend text-2xl text-left  label">Email:</legend>
-          <input type="email" className="input w-full" />
-        </fieldset>
-        <div className="w-full  h-auto flex justify-center items-center">   
-                <button className="btn btn-primary  label ">Submit</button>
-        </div>  
+    <div className="w-[100%] h-[100vh] bg-white ">
+      <div className="w-[100%] h-[85vh] container-S p-1 m-1 shadow-2xl m-2 rounded ">
+        <h1 className="text-center text-4xl">Addmission From</h1>
+        <div className="grid grid-cols-4 max-[1200px]:grid-cols-3 max-[800px]:grid-cols-2 max-[450px]:grid-cols-1">
+          <fieldset className="fieldset  m-2  w-64   max-[850px]:w-52 max-[650px]:w-48 max-[450px]:w-full">
+            <legend className="fieldset-legend text-left text-xl max-[850px]:text-base max-[650px]:text-sm max-[450px]:text-xl">Student's First Name:</legend>
+            <input type="text" className="input " placeholder="Type here" />
+
+          </fieldset>
+          <fieldset className="fieldset  m-2 w-64 max-[850px]:w-52 max-[650px]:w-48 max-[450px]:w-full ">
+            <legend className="fieldset-legend text-left text-xl max-[850px]:text-base max-[650px]:text-sm max-[450px]:text-xl">Student's Last Name:</legend>
+            <input type="text" className="input  " placeholder="Type here" />
+
+          </fieldset>
+          <fieldset className="fieldset  m-2 w-64  max-[850px]:w-52 max-[650px]:w-48 max-[450px]:w-full ">
+            <legend className="fieldset-legend text-left text-xl max-[850px]:text-base max-[650px]:text-sm max-[450px]:text-xl">Student's Date of Birth:</legend>
+            <input type="date" className="input  " placeholder="Type here" />
+
+          </fieldset>
+          <fieldset className="fieldset  m-2 w-64  max-[850px]:w-52 max-[650px]:w-48 max-[450px]:w-full ">
+            <legend className="fieldset-legend text-left text-xl max-[850px]:text-base max-[650px]:text-sm max-[450px]:text-xl">class you want to apply for:</legend>
+            <input type="text" className="input  " placeholder="Type here" />
+
+          </fieldset>
+        </div>
 
       </div>
     </div>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,11 +5,11 @@ const Login = () => {
         <div className="w-full h-[100vh] bg-white flex justify-center items-center">
             <div className="w-96 h-80 bg-white rounded text-center p-1 m-2 shadow-xl">
                 <h1 className="text-2xl">Sign In</h1>
-                <fieldset className="fieldset flex justify-center items-center ">
-                    <legend className="fieldset-legend text-left text-xl ">Email</legend>
-                    <input type="Email" className="input w-full " placeholder="Type here" />
-                    {/* <p className="label">Optional</p> */}
-                </fieldset>
+                    <fieldset className="fieldset flex justify-center items-center ">
+                        <legend className="fieldset-legend text-left text-xl ">Email</legend>
+                        <input type="Email" className="input w-full " placeholder="Type here" />
+                        {/* <p className="label">Optional</p> */}
+                    </fieldset>
                 <fieldset className="fieldset flex justify-center items-center ">
                     <legend className="fieldset-legend text-left text-xl ">Password</legend>
                     <input  type="Password" className="input w-full" placeholder="Type here" />
